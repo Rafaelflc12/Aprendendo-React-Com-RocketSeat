7a00fcc0b4222e341e456db9c41aff979756cd9d
Rafael Colonese <rafaelflcolonese2@hotmail.com>
2021-06-25 20:51:21 -0300

    quase finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { AdminRoom } from './components/AdminRoom';
 import { AuthContextProvider } from './contexts/AuthContext';
 
  import { Home } from './pages/Home';
@@ -17,6 +18,9 @@ function App() {
       <Route path="/" exact component={Home} /> 
     <Route path="/rooms/new" component={NewRoom} /> 
     <Route path="/rooms/:id" component={Room} /> 
+
+
+    <Route path="/admin/rooms/:id" component={AdminRoom} />
     </Switch>
     </AuthContextProvider>
     </BrowserRouter>
